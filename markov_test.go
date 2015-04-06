b54211cdb07c7691a5a2b8d9e7096a290dd0b17c
Joshua Harrison <joshharrison@gmail.com>
2015-04-05 23:22:00 -0700

    Fix build cycle by moving the test into a separate package.

--- markov_test.go
+++ markov_test.go
@@ -1,15 +1,15 @@
-package markov
+package markov_test
 
 import (
 	"regexp"
 	"testing"
 
-	"github.com/husafan/markov"
+	. "github.com/husafan/markov"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUintModelCanOnlyAcceptUintTypes(t *testing.T) {
-	model := markov.NewUintModel()
+	model := NewUintModel()
 	assert.NotNil(t, model)
 
 	var err error
@@ -26,23 +26,23 @@ func TestUintModelCanOnlyAcceptUintTypes(t *testing.T) {
 }
 
 func TestUintModelSizeCalculations(t *testing.T) {
-	model := markov.NewUintModel()
+	model := NewUintModel()
 
 	var err error
-	err = model.AddData(markov.Uint16(32))
+	err = model.AddData(Uint16(32))
 	assert.Nil(t, err)
 	// The model should have a single entry from 0 to 32 with a count of 1.
 	// This means 1 byte for the 0, 2 bytes for the uint16 and 1 byte for
 	// the count of 1.
 	assert.Equal(t, uint64(4), model.Size())
 
-	err = model.AddData(markov.Uint16(32))
+	err = model.AddData(Uint16(32))
 	assert.Nil(t, err)
 	// Two new entries of size uint16 and a new count of size uint8 adds
 	// another 5 bytes to the size.
 	assert.Equal(t, uint64(9), model.Size())
 
-	err = model.AddData(markov.Uint16(32))
+	err = model.AddData(Uint16(32))
 	assert.Nil(t, err)
 	// This entry simply increments the count, so no added size.
 	assert.Equal(t, uint64(9), model.Size())
