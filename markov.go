d3f607f58f6a4d2cf1983cc602b93feecebd55e4
Joshua Harrison <joshharrison@gmail.com>
2015-04-12 12:00:31 -0700

    Transitioned the NormalizingRow to use a count instead of the weight.
    Rather than try to keep a current weight, the NormalizingRow now keeps a count
    of the number of times a specific state has been seen as well as the number of
    total states transitioned to. This allows for counting state transitions without
    having to renormalize weights while building the model.

--- markov.go
+++ markov.go
@@ -3,9 +3,6 @@ package markov
 // A special start state.
 var start State = StringState("start")
 
-// A constant that represents the value of 1.
-const oneValue uint32 = 1000000000
-
 /**
  * The State interface interface must be implemented by all types of states
  * added to a Markov model. Note that implementations of this interface will
@@ -23,43 +20,49 @@ type State interface {
  */
 type Model struct {
 	last State
-	size uint64
 	data map[State]*NormalizingRow
 }
 
-func (m *Model) AddState(state *State) {
+func (m *Model) AddState(state State) {
 	_, ok := m.data[m.last]
 	if !ok {
 		m.data[m.last] = NewNormalizingRow()
 	}
+	row := m.data[m.last]
+	row.AddState(state)
+	m.last = state
 }
 
 func (m *Model) Size() uint64 {
-	return uint64(0)
+	var size uint64 = 0
+	for key, value := range m.data {
+		size += key.Size() + value.Size()
+	}
+	return size
 }
 
 func NewModel() *Model {
-	return &Model{start, uint64(0), make(map[State]*NormalizingRow)}
+	return &Model{start, make(map[State]*NormalizingRow)}
 }
 
 /**
  * In a given model, the transitons between a given state and its successive
  * states is represented as set of probabilities between 0 and 1. This object
  * encapsulates the mechanics of adding transition states to a given starting
- * state and re-balancing the weights so that the probabilities remain
- * normalized.
+ * state and retreiving the weights associated with any given state.
  */
 type NormalizingRow struct {
-	// Holds the weight of the next value added to this row. Beause the
-	// weight is between 0 and 1, this value represents the digits after
-	// the decimal.
-	currentWeight uint32
-	// Holds the transition probabilities.
+	// Holds the number of states that have been added to this row.
+	currentCount uint32
+	// Holds the counts for given state transitions.
 	rowData map[State]uint32
 	// Holds the current size of this row.
 	size uint64
 }
 
+/**
+ * Returns the current size of the row in bytes.
+ */
 func (n *NormalizingRow) Size() uint64 {
 	return n.size
 }
@@ -71,27 +74,28 @@ func (n *NormalizingRow) Size() uint64 {
  */
 func (n *NormalizingRow) AddState(state State) uint64 {
 	value := n.rowData[state]
-	value = value + n.currentWeight
-	n.rowData[state] = value
-	n.size = uint64(4)
-	for key, value := range n.rowData {
-		if key != state {
-			n.rowData[key] = value - n.currentWeight
-		}
-		n.size = n.size + key.Size()
+	// If value is 0, this state has not been seen before. Add the size of
+	// the key plus the size of its counter to the current size.
+	if value == 0 {
+		n.size += state.Size() + 4
 	}
-	n.currentWeight = n.currentWeight / 2
+	value = value + 1
+	n.rowData[state] = value
+	n.currentCount++
 	return n.size
 }
 
 /**
  * Returns the weight associated with a given state, and true if the state
  * existed in the underlying data. If the state has not been seen before, this
- * method returns 0 and false.
+ * method returns 0.
  */
-func (n *NormalizingRow) StateWeight(state State) (uint32, bool) {
-	value, ok := n.rowData[state]
-	return value, ok
+func (n *NormalizingRow) StateWeight(state State) float64 {
+	value := n.rowData[state]
+	if value == 0 {
+		return float64(value)
+	}
+	return float64(value) / float64(n.currentCount)
 }
 
 /**
@@ -99,5 +103,5 @@ func (n *NormalizingRow) StateWeight(state State) (uint32, bool) {
  * weight equal to 1 and a size accounting only for the weight value.
  */
 func NewNormalizingRow() *NormalizingRow {
-	return &NormalizingRow{oneValue, make(map[State]uint32), uint64(4)}
+	return &NormalizingRow{0, make(map[State]uint32), uint64(4)}
 }
--- markov_test.go
+++ markov_test.go
@@ -31,22 +31,93 @@ func TestStringState(t *testing.T) {
 func TestNormalizingRowReturnsNotOkForNonValue(t *testing.T) {
 	row := NewNormalizingRow()
 	state := Uint16State(32)
-	value, ok := row.StateWeight(state)
-	assert.Equal(t, uint64(0), value)
-	assert.Equal(t, false, ok)
+	value := row.StateWeight(state)
+	assert.Equal(t, float64(0), value)
 }
 
 func TestNormalizingRowSize(t *testing.T) {
 	row := NewNormalizingRow()
 	assert.Equal(t, uint64(4), row.Size())
 
-	// A Uint16State has a size of 2 bytes. So after adding it to the row
-	// the new size should be 6.
+	// A Uint16State has a size of 2 bytes plus a 4 byte counts. So after
+	// adding it to the row the new size should be 10.
 	uintState := Uint16State(32)
-	assert.Equal(t, uint64(6), row.AddState(uintState))
+	assert.Equal(t, uint64(10), row.AddState(uintState))
 
 	// The following StringState has 1 byte per character, so the new total
-	// size should be 6 + 5 = 11.
+	// size should be 10 + 5 + 4 = 19.
 	stringState := StringState("Hello")
-	assert.Equal(t, uint64(11), row.AddState(stringState))
+	assert.Equal(t, uint64(19), row.AddState(stringState))
+}
+
+func TestCannotBeWeightedMoreThanOneValue(t *testing.T) {
+	state16 := Uint16State(16)
+	row := NewNormalizingRow()
+
+	row.AddState(state16)
+	assert.Equal(t, float64(1), row.StateWeight(state16))
+	row.AddState(state16)
+	assert.Equal(t, float64(1), row.StateWeight(state16))
+}
+
+func TestNormalizingRowWeighting(t *testing.T) {
+	state16 := Uint16State(16)
+	state32 := Uint16State(32)
+	state64 := Uint16State(64)
+	row := NewNormalizingRow()
+
+	row.AddState(state16)
+	assert.Equal(t, float64(1), row.StateWeight(state16))
+
+	row.AddState(state32)
+	assert.Equal(t, float64(1)/float64(2), row.StateWeight(state16))
+	assert.Equal(t, float64(1)/float64(2), row.StateWeight(state32))
+
+	row.AddState(state64)
+	assert.Equal(t, float64(1)/float64(3), row.StateWeight(state16))
+	assert.Equal(t, float64(1)/float64(3), row.StateWeight(state32))
+	assert.Equal(t, float64(1)/float64(3), row.StateWeight(state64))
+
+	row.AddState(state64)
+	assert.Equal(t, float64(1)/float64(4), row.StateWeight(state16))
+	assert.Equal(t, float64(1)/float64(4), row.StateWeight(state32))
+	assert.Equal(t, float64(2)/float64(4), row.StateWeight(state64))
+
+	row.AddState(state32)
+	assert.Equal(t, float64(1)/float64(5), row.StateWeight(state16))
+	assert.Equal(t, float64(2)/float64(5), row.StateWeight(state32))
+	assert.Equal(t, float64(2)/float64(5), row.StateWeight(state64))
+}
+
+func TestModelSize(t *testing.T) {
+	state16 := Uint16State(16)
+	state32 := Uint16State(32)
+	model := NewModel()
+
+	model.AddState(state16)
+	// The start state is "start" which is 5 bytes for the key. The state16
+	// is 2 bytes for the key and 4 bytes for the counter and 4 bytes for the
+	// current count of the row. 5 + 2 + 4 + 4 = 15.
+	assert.Equal(t, uint64(15), model.Size())
+
+	model.AddState(state32)
+	// 2 more bytes for state16 as a State key for the start state plus
+	// another 2 + 4 + 4 bytes for state32 as a State key to be transitioned
+	// to, its counter and the new row's total count.
+	assert.Equal(t, uint64(27), model.Size())
+
+	model.AddState(state32)
+	// 2 more bytes for state32 as a State key for the start state plus
+	// another 2 + 4 + 4 bytes for state32 as a State key to be transitioned
+	// to, its counter and the new row's total count.
+	assert.Equal(t, uint64(39), model.Size())
+
+	model.AddState(state32)
+	// No new data, as we've already seen state32 -> state32.
+	assert.Equal(t, uint64(39), model.Size())
+
+	model.AddState(state16)
+	// state16 as a transition state means 2 more bytes for the key and 4
+	// more bytes for the counter.
+	assert.Equal(t, uint64(45), model.Size())
 }
